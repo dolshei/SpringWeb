6e59718d1d620ecb8bb958c78946a72c373bab97
Argali <dolshei@gmail.com>
2021-07-03 23:25:58 +0900

    Sping Boot 2.x Quick Start 강의 22 까지

--- src/main/java/com/newlecture/web/controller/admin/board/NoticeController.java
+++ src/main/java/com/newlecture/web/controller/admin/board/NoticeController.java
@@ -1,22 +1,33 @@
 package com.newlecture.web.controller.admin.board;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.newlecture.web.entity.Notice;
+import com.newlecture.web.service.NoticeService;
 
 @Controller("adminNoticeController")
 @RequestMapping("/admin/board/notice/")
 public class NoticeController {
 
-	@RequestMapping("list")
-	public String list(Model model) {
+	@Autowired
+	private NoticeService service;		//controller 가 필요로 하는 interface
 
-		model.addAttribute("test", "Hello~ ");
+	@RequestMapping("list")
+	public String list() {
+
+		List<Notice> list = service.getList();
+
 		return "admin.board.notice.list";
 	}
 
 	@RequestMapping("detail")
 	public String detail() {
+
+		Notice notice = service.get(1);
 		return "admin.board.notice.detail";
 	}
 
