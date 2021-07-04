692213f404e8961681de4b93789213b015121ba7
Argali <dolshei@gmail.com>
2021-07-04 22:59:34 +0900

    newlec 23강까지

--- src/main/java/com/newlecture/web/controller/customer/NoticeController.java
+++ src/main/java/com/newlecture/web/controller/customer/NoticeController.java
@@ -1,17 +1,28 @@
 package com.newlecture.web.controller.customer;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.newlecture.web.entity.Notice;
+import com.newlecture.web.service.NoticeService;
 
 @Controller
 @RequestMapping("/customer/notice/")
 public class NoticeController {
 
+	@Autowired
+	private NoticeService service;
+
 	@RequestMapping("list")
 	public String list(Model model) {
 
-		model.addAttribute("test", "Hello~ ");
+		List<Notice> list = service.getList();
+
+		model.addAttribute("list", list);
 		//return "customer/notice/list";
 		return "customer.notice.list";
 	}
